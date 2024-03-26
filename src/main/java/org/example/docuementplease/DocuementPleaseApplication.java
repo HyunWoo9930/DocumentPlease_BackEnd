c24b29793277c260ed48de3214aa7d6001bbb630
HyunWoo9930 <hw62459930@gmail.com>
2024-03-26 21:06:51 +0900

    [ADD] Spring Boot intial file 추가

---
+++ src/main/java/org/example/docuementplease/DocuementPleaseApplication.java
@@ -0,0 +1,13 @@
+package org.example.docuementplease;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocuementPleaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DocuementPleaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/docuementplease/DocuementPleaseApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.docuementplease;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocuementPleaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
